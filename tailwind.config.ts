f4425a980d179a5a0a6f81882280b948ad2a97af
svsm <srivathsamurthys.1745@gmail.com>
2025-07-19 20:57:36 +0530

    update 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ import tailwindcssAnimate from "tailwindcss-animate";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
+		// "./pages/**/*.{ts,tsx}",
+		// "./components/**/*.{ts,tsx}",
+		// "./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
 	],
 	prefix: "",
